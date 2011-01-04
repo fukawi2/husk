697e903e4eb2edf465c607f771053a50b0d0ffc4
Phillip Smith <fukawi2@gmail.com>
2011-01-05 09:15:37 +1100

    updated logging options for log and drop

--- husk.pl
+++ husk.pl
@@ -738,7 +738,7 @@ sub log_and_drop {
 	&bomb((caller(0))[3] . ' called without passing $chain') unless $chain;
 
 	# LOG the packet
-	&ipt(&collapse_spaces(sprintf('%s -A %s %s -m limit --limit 1/second -j LOG --log-prefix="[%s] " --log-tcp-sequence --log-tcp-options --log-ip-options',
+	&ipt(&collapse_spaces(sprintf('%s -A %s %s -m limit --limit 1/second -j LOG --log-prefix="[%s] "',
 			$table, $chain, $criteria, $log_prefix,
 		)));
 	# DROP the packet
