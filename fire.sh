365b0fd512a99f2c332f4240db490e6772c124b2
Phillip Smith <fukawi2@gmail.com>
2011-04-11 16:42:36 +1000

    cleanup of fire script

--- fire.sh
+++ fire.sh
@@ -28,44 +28,20 @@ fi
 args=("$@")
 
 TIMEOUT=10
-IPT_SAVE=`which iptables-save 2>/dev/null`
-IPT_RESTORE=`which iptables-restore 2>/dev/null`
-HUSK=`which husk 2>/dev/null`
-TFILE=`mktemp -t husk-fire.XXX`
-SFILE=`mktemp -t husk-fire-save.XXX`
 
 trap "rm -f $TFILE; rm -f $SFILE" EXIT 1 2 3 4 5 6 7 8 10 11 12 13 14 15
 
 # Check we've got all our dependencies
-if [ ! -e "$HUSK" ] ; then
-	echo "Could not locate 'husk'" >&2
-	exit 1
-fi
-if [ ! -e "$IPT_SAVE" ] ; then
-	echo "Could not locate 'iptables-save'" >&2
-	exit 1
-fi
-if [ ! -e "$IPT_RESTORE" ] ; then
-	echo "Could not locate 'iptables-restore'" >&2
-	exit 1
-fi
-# ... and that they are executable
-if [ ! -x "$HUSK" ] ; then
-	echo "Found 'husk' but it is not executable: $HUSK" >&2
-	exit 2
-fi
-if [ ! -x "$IPT_SAVE" ] ; then
-	echo "Found 'iptables-save' but it is not executable: $IPT_SAVE" >&2
-	exit 2
-fi
-if [ ! -x "$IPT_RESTORE" ] ; then
-	echo "Found 'iptables-restore' but it is not executable: $IPT_RESTORE" >&2
-	exit 2
-fi
+export PATH='/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin'
+for ebin in iptables-save iptables-restore husk mktemp cat grep ; do
+	[[ -z "$(which $ebin 2>/dev/null)" ]] && { echo "Could not locate '$ebin'" >&2; exit 1; }
+done
+TFILE=$(mktemp -t husk-fire.XXX)
+SFILE=$(mktemp -t husk-fire-save.XXX)
 
 # Compile ruleset to a temporary file
 echo 'Compiling rules.... '
-if "$HUSK" $@ &> "$TFILE" ; then
+if "husk" $@ &> "$TFILE" ; then
     echo '   DONE'
 else
     echo 'Error compiling ruleset:' >&2
@@ -75,7 +51,7 @@ fi
 
 # Save current ruleset to a temporary file
 echo "Saving current rules.... "
-if "$IPT_SAVE" > "$SFILE" ; then
+if iptables-save > "$SFILE" ; then
     echo '   DONE'
 else
 	if ! grep -q ipt /proc/modules 2>/dev/null ; then
@@ -104,7 +80,7 @@ if [ "${args[0]}" != '--no-confirm' ] ; then
 				echo "Uh-oh... Timeout waiting for reply!" >&2
 			fi
 			echo "Reverting to saved rules..." >&2
-			"$IPT_RESTORE" < "$SFILE";
+			iptables-restore < "$SFILE";
 			exit 255
 			;;
 	esac
