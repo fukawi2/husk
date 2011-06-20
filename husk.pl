d4f3daa26bf35543d0b34022d5fbc056f966e2bb
Phillip Smith <phillip.smith@naturesorganics.com.au>
2011-06-20 13:54:33 +1000

    updated copyright statements

--- husk.pl
+++ husk.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-# Copyright (C) 2010 Phillip Smith
+# Copyright (C) 2010-2011 Phillip Smith
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
