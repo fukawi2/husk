dd863f3fa67fd1f5d4d104a518c3c44a825eddae
Phillip Smith <fukawi2@gmail.com>
2011-08-11 21:56:43 +1000

    added package main to top of file

--- src/husk.pl
+++ src/husk.pl
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+package main;
+
 use warnings;
 use strict;
 #use 5.010_001;	# Need Perl version 5.10 for Coalesce operator (//)
