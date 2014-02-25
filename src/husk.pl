2d3a8aca4cdb1c886cbc4734d7249de90660b2e7
Phillip Smith <fukawi2@gmail.com>
2014-02-25 13:27:34 +1100

    remove useless code

--- src/husk.pl
+++ src/husk.pl
@@ -308,7 +308,7 @@ sub read_rules_file {
 
       # make sure the user isn't trying to use a reserved word
       bomb(sprintf('Target "%s" is named the same as a reserved word. This is invalid', $udc_name))
-        if ( grep { $_ =~ /$udc_name/i } @RESERVED_WORDS );
+        if ( grep { m/$udc_name/i } @RESERVED_WORDS );
 
       $curr_chain = new_udc_chain(line=>$line, udc_name=>$udc_name);
     }
